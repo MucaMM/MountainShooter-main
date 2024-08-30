62e27dc252efc2592d3c40e8d16fdc55ff187c1f
Muca <43623426+MuriloMaranzati@users.noreply.github.com>
2024-08-30 15:03:56 -0300

    test_colorwhite

--- code/Menu.py
+++ code/Menu.py
@@ -21,7 +21,7 @@ class Menu:
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Mountain", C_WHITE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
